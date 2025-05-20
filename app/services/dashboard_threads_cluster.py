e72ee65fc1c29a3ed07ad5318fc3df68cbae256c
Mister-proust <maximilien_proust@hotmail.fr>
2025-05-20 15:27:11 +0200

    Ajout du service non fonctionnel

---
+++ app/services/dashboard_threads_cluster.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from bertopic import BERTopic
+
+# Chargement des objets une seule fois
+model = BERTopic.load(".../data/bertopic_original_model")
+df = pd.read_csv(".../data/df_questions.csv")
+
+# Fonction pour obtenir les clusters
+def get_all_clusters():
+    topics_info = model.get_topic_info()
+    return topics_info.to_dict(orient="records")
+
+# Fonction pour récupérer les messages d'un topic donné
+def get_messages_by_topic(topic_id: int):
+    topic_assignments = model.topics_
+    df["topic_id"] = topic_assignments
+
+    filtered_df = df[df["topic_id"] == topic_id]
+    return filtered_df[["contenu_message", "titre_sujet"]].to_dict(orient="records")
+
+# Fonction pour obtenir les mots-clés d’un topic
+def get_topic_keywords(topic_id: int):
+    topic = model.get_topic(topic_id)
+    return [word for word, _ in topic]
